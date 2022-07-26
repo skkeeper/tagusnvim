71586e8474f9d61f5fd0945629dddbe61e38d61f
Fábio André Damas <fabio.damas@morphotech.co.uk>
2022-07-26 17:27:11 +0100

    mason: check if runtimes exist before installing lsp/tools

--- lua/tagusnvim/lsp/mason-lspconfig.lua
+++ lua/tagusnvim/lsp/mason-lspconfig.lua
@@ -4,8 +4,6 @@ M.bootstrap = function()
     require('mason').setup()
     local required_servers = {
         'sumneko_lua',
-        'tsserver',
-        'eslint',
         'cssls',
         'jsonls',
         'html',
@@ -15,12 +13,18 @@ M.bootstrap = function()
         'quick_lint_js',
     }
     local mason_lspconfig = require('mason-lspconfig')
+
+    local is_present = require('tagusnvim.is_present')
+    if is_present('node --version') then
+        table.insert(required_servers, 'eslint')
+        table.insert(required_servers, 'tsserver')
+    end
+
     mason_lspconfig.setup({
         ensure_installed = required_servers,
     })
 
-    local installed_servers =
-        mason_lspconfig.get_installed_servers()
+    local installed_servers = mason_lspconfig.get_installed_servers()
     for k, server in ipairs(installed_servers) do
         local capabilities = require('cmp_nvim_lsp').update_capabilities(
             vim.lsp.protocol.make_client_capabilities()
@@ -57,18 +61,28 @@ M.bootstrap = function()
 end
 
 M.tool_installer = function()
+    local ensure_installed = {
+        'bash-language-server',
+        'lua-language-server',
+        'vim-language-server',
+        'shellcheck',
+        'editorconfig-checker',
+        'luacheck',
+        'shellcheck',
+    }
+
+    local is_present = require('tagusnvim.is_present')
+    if is_present('go version') then
+        table.insert(ensure_installed, 'golangci-lint')
+        table.insert(ensure_installed, 'gopls')
+        table.insert(ensure_installed, 'revive')
+    end
+    if is_present('cargo version') then
+        table.insert(ensure_installed, 'stylua')
+    end
+
     require('mason-tool-installer').setup({
-        ensure_installed = {
-            'bash-language-server',
-            'lua-language-server',
-            'vim-language-server',
-            'gopls',
-            'shellcheck',
-            'editorconfig-checker',
-            'luacheck',
-            'revive',
-            'shellcheck',
-        },
+        ensure_installed = ensure_installed,
     })
 end
 
